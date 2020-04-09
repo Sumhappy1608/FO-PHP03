d8c2fe1c33ea5f84a6492ecf0678edf8f2009a86
Sumhappy1608 <50817071+Sumhappy1608@users.noreply.github.com>
2020-04-09 22:35:05 +0700

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+
+$username = "root";  //Ten dang nhap
+
+$password = "";   //Mat khau truy cap
+
+$dbname = "blogs";   //Ten co so du lieu muon ket noi den
+
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+?>
--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php
 
-$servername = "localhost";
-
-$username = "root";  //Ten dang nhap
-
-$password = "";   //Mat khau truy cap
-
-$dbname = "blogs";   //Ten co so du lieu muon ket noi den
-
-
-//Tao ket noi co so du lieu
-
-$conn = new mysqli($servername,$username,$password,$dbname);
+require_once('connection.php');
 
 //Load category
 $query_category = "SELECT * FROM categories";
@@ -46,7 +35,7 @@ while($row = $result_two_posts->fetch_assoc())
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		 <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-		<title>WebMag HTML Template</title>
+		<title>My first blog</title>
 
 		<!-- Google font -->
 		<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:700%7CNunito:300,600" rel="stylesheet"> 
@@ -73,112 +62,7 @@ while($row = $result_two_posts->fetch_assoc())
 		<!-- Header -->
 		<header id="header">
 			<!-- Nav -->
-			<div id="nav">
-				<!-- Main Nav -->
-				<div id="nav-fixed">
-					<div class="container">
-						<!-- logo -->
-						<div class="nav-logo">
-							<a href="index.html" class="logo"><img src="./img/logo.png" alt=""></a>
-						</div>
-						<!-- /logo -->
-
-						<!-- nav -->
-						<ul class="nav-menu nav navbar-nav">
-							<!--<li><a href="category.html">News</a></li>
-							<li><a href="category.html">Popular</a></li>
-							<li class="cat-1"><a href="category.html">Web Design</a></li>
-							<li class="cat-2"><a href="category.html">JavaScript</a></li>
-							<li class="cat-3"><a href="category.html">Css</a></li>-->
-							<?php
-							$i = 1; 
-
-							foreach($categories as $cate){ 
-							?>
-							    <li class="cat-<?= $i?>"><a href="category.html"><?= $cate['Title'] ?></a></li>
-							<?php 
-							$i++;
-							if($i == 5) $i=1;
-						    }
-						    ?>
-						</ul>
-						<!-- /nav -->
-						<? php
-					    }
-						?>
-
-						<!-- search & aside toggle -->
-						<div class="nav-btns">
-							<button class="aside-btn"><i class="fa fa-bars"></i></button>
-							<button class="search-btn"><i class="fa fa-search"></i></button>
-							<div class="search-form">
-								<input class="search-input" type="text" name="search" placeholder="Enter Your Search ...">
-								<button class="search-close"><i class="fa fa-times"></i></button>
-							</div>
-						</div>
-						<!-- /search & aside toggle -->
-					</div>
-				</div>
-				<!-- /Main Nav -->
-
-				<!-- Aside Nav -->
-				<div id="nav-aside">
-					<!-- nav -->
-					<div class="section-row">
-						<ul class="nav-aside-menu">
-							<li><a href="index.html">Home</a></li>
-							<li><a href="about.html">About Us</a></li>
-							<li><a href="#">Join Us</a></li>
-							<li><a href="#">Advertisement</a></li>
-							<li><a href="contact.html">Contacts</a></li>
-						</ul>
-					</div>
-					<!-- /nav -->
-
-					<!-- widget posts -->
-					<div class="section-row">
-						<h3>Recent Posts</h3>
-						<div class="post post-widget">
-							<a class="post-img" href="blog-post.html"><img src="./img/widget-2.jpg" alt=""></a>
-							<div class="post-body">
-								<h3 class="post-title"><a href="blog-post.html">Pagedraw UI Builder Turns Your Website Design Mockup Into Code Automatically</a></h3>
-							</div>
-						</div>
-
-						<div class="post post-widget">
-							<a class="post-img" href="blog-post.html"><img src="./img/widget-3.jpg" alt=""></a>
-							<div class="post-body">
-								<h3 class="post-title"><a href="blog-post.html">Why Node.js Is The Coolest Kid On The Backend Development Block!</a></h3>
-							</div>
-						</div>
-
-						<div class="post post-widget">
-							<a class="post-img" href="blog-post.html"><img src="./img/widget-4.jpg" alt=""></a>
-							<div class="post-body">
-								<h3 class="post-title"><a href="blog-post.html">Tell-A-Tool: Guide To Web Design And Development Tools</a></h3>
-							</div>
-						</div>
-					</div>
-					<!-- /widget posts -->
-
-					<!-- social links -->
-					<div class="section-row">
-						<h3>Follow us</h3>
-						<ul class="nav-aside-social">
-							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
-							<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-							<li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-						</ul>
-					</div>
-					<!-- /social links -->
-
-					<!-- aside nav close -->
-					<button class="nav-aside-close"><i class="fa fa-times"></i></button>
-					<!-- /aside nav close -->
-				</div>
-				<!-- Aside Nav -->
-			</div>
+			<?php require_once('MainNav.php'); ?>
 			<!-- /Nav -->
 		</header>
 		<!-- /Header -->
@@ -189,35 +73,26 @@ while($row = $result_two_posts->fetch_assoc())
 			<div class="container">
 				<!-- row -->
 				<div class="row">	
-					<!-- post -->
-					<div class="col-md-6">
-						<div class="post post-thumb">
-							<a class="post-img" href="blog-post.html"><img src="./img/post-1.jpg" alt=""></a>
-							<div class="post-body">
-								<div class="post-meta">
-									<a class="post-category cat-2" href="category.html">JavaScript</a>
-									<span class="post-date">March 27, 2018</span>
-								</div>
-								<h3 class="post-title"><a href="blog-post.html">Chrome Extension Protects Against JavaScript-Based CPU Side-Channel Attacks</a></h3>
-							</div>
-						</div>
-					</div>
-					<!-- /post -->
+					<?php foreach($two_posts as $post){
 
+					?>
 					<!-- post -->
 					<div class="col-md-6">
 						<div class="post post-thumb">
-							<a class="post-img" href="blog-post.html"><img src="./img/post-2.jpg" alt=""></a>
+							<a class="post-img" href="blog-post.php?ID=<?= $post['ID'] ?>"><img src="<?= $post['Thumbnail'] ?>" alt="" height="350px"></a>
 							<div class="post-body">
 								<div class="post-meta">
-									<a class="post-category cat-3" href="category.html">Jquery</a>
-									<span class="post-date">March 27, 2018</span>
+									<a class="post-category cat-2" href="category.html"><?= $post['categories'] ?></a>
+									<span class="post-date"><?= $post['Created_at']   ?></span>
 								</div>
-								<h3 class="post-title"><a href="blog-post.html">Ask HN: Does Anybody Still Use JQuery?</a></h3>
+								<h3 class="post-title"><a href="blog-post.php?ID=<?= $post['ID'] ?>"><?= $post['Title']   ?></a></h3>
 							</div>
 						</div>
 					</div>
 					<!-- /post -->
+					<?php } ?>
+
+					
 				</div>
 				<!-- /row -->
 
